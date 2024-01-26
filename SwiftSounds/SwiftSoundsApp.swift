3d3de3a598f0bb6c83c346f89b860139a729da81
Charlene Hoareau <charlene.hoareau@outlook.com>
2024-01-26 16:42:50 +0100

    Initial Commit

---
+++ SwiftSounds/SwiftSoundsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftSoundsApp.swift
+//  SwiftSounds
+//
+//  Created by charlene hoareau on 26/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftSoundsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
